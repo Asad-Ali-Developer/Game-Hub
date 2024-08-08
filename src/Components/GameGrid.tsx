ee91a4edb0b2b3cbf6d069a7baafcd861f79ffcb
Asad <uhope1645@gmail.com>
2024-08-08 17:44:15 +0500

    Refactoring the game grid

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -13,12 +13,11 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
     const { data, error, isLoading } = useGames(gameQuery)
     const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+        if(error) return <Text>{error}</Text>
+    
     return (
-        <div>
-
-            {error && <Text>{error}</Text>}
-
-
+    
             <SimpleGrid
                 columns={{ sm: 2, md: 2, lg: 3, xl: 4 }}
                 spacing={10}
@@ -40,7 +39,6 @@ const GameGrid = ({ gameQuery }: Props) => {
 
             </SimpleGrid>
 
-        </div >
     )
 }
 
