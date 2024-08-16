beb9fc1e4b22432d300d6b560109e919fc45c062
Asad <uhope1645@gmail.com>
2024-08-16 17:43:55 +0500

    Did Change in Logo Name

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = ({ onSearch } : Props) => {
         <HStack justifyContent='space-between' gap={3} padding='10px' fontFamily='sans-serif'>
           
           <Box width={10} height={10} display={'flex'} justifyContent={'center'} alignItems={'center'} fontSize={'xl'} fontWeight={'bold'} border={'1px'} borderRadius={10}
-          borderColor={'gray.700'} >T</Box>
+          borderColor={'gray.700'} >A</Box>
 
           <SearcHBar onSearch={onSearch} />
           <ColorModeSwitch />
